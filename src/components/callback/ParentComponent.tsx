a9fe1b8d9c4069abf089b10b7f0a5279454128d7
sehuikim02 <lulla1613@gmail.com>
2025-09-04 13:16:55 +0900

    usecallback 예제

--- src/components/callback/ParentComponent.tsx
+++ src/components/callback/ParentComponent.tsx
@@ -6,21 +6,21 @@ function ParentComponent() {
     const [age, setAge] = useState(25)
     const [salary, setSalary] = useState(50000)
 
-    // const incrementAge = useCallback(() => {
-    // 	setAge(age + 1)
-    // }, [age])
+    const incrementAge = useCallback(() => {
+    	setAge(age + 1)
+    }, [age])
 
-    const incrementAge = () => {
-        setAge(age + 1)
-    }
+    // const incrementAge = () => {
+    //     setAge(age + 1)
+    // }
 
-    // const incrementSalary = useCallback(() => {
-    // 	setSalary(salary + 1000)
-    // }, [salary])
+    const incrementSalary = useCallback(() => {
+    	setSalary(salary + 1000)
+    }, [salary])
 
-    const incrementSalary = () => {
-        setSalary(salary + 1000)
-    }
+    // const incrementSalary = () => {
+    //     setSalary(salary + 1000)
+    // }
 
     return (
         <div>
